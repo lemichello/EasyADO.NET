ecc6315b856da5ce340871c66d705fd0168f705a
Lemich Maksym <lemichello.m@gmail.com>
2019-09-04 14:18:19 +0300

    feat(*): added Update method, which updates given values in the table by conditions

--- EasyADO.NET/EasyAdoNetUpdate.cs
+++ EasyADO.NET/EasyAdoNetUpdate.cs
@@ -37,6 +37,42 @@ namespace EasyADO.NET
             }
         }
 
+        /// <summary>
+        /// Updates given values in the given table by given conditions.
+        /// </summary>
+        /// <param name="tableName">Name of the table, in which will be updating.</param>
+        /// <param name="equalityConditions">Conditions, by which will be comparing in 'WHERE' section. First component - name of the column,
+        /// second element - value of the column.</param>
+        /// <param name="updatingValues">Values, which will be replaced in the table. First component - name of the column, second component - value.</param>
+        /// <exception cref="ArgumentException">Throws, when given <paramref name="tableName"/> doesn't exist in the database or <paramref name="equalityConditions"/> or <paramref name="updatingValues"/> are empty.</exception>
+        /// <exception cref="ArgumentNullException">Throws, when one of the parameters is null.</exception>
+        /// <exception cref="SqlException">Throws, when <paramref name="equalityConditions"/> or <paramref name="updatingValues"/> have non-existing column.</exception>
+        public void Update(string tableName, Tuple<string, object>[] equalityConditions,
+            params Tuple<string, object>[] updatingValues)
+        {
+            CheckForTableExistent(tableName);
+            CheckUpdatingValues(updatingValues);
+            CheckConditions(equalityConditions);
+
+            var connection = GetAndOpenConnection();
+
+            using (var command = new SqlCommand(BuildUpdateQuery(tableName, equalityConditions, updatingValues),
+                connection))
+            {
+                foreach (var (column, value) in updatingValues)
+                {
+                    command.Parameters.AddWithValue($"@{column}", value);
+                }
+
+                foreach (var (column, value) in equalityConditions)
+                {
+                    command.Parameters.AddWithValue($"@eq{column}", value);
+                }
+
+                command.ExecuteNonQuery();
+            }
+        }
+
         private void CheckUpdatingValues(Tuple<string, object>[] updatingValues)
         {
             try
@@ -66,6 +102,27 @@ namespace EasyADO.NET
             var res = builder.ToString().Substring(0, builder.Length - 2);
 
             return $"{res} {predicate}";
+        }
+
+        private static string BuildUpdateQuery(string tableName, Tuple<string, object>[] equalityConditions,
+            Tuple<string, object>[] updatingValues)
+        {
+            var builder = new StringBuilder($"UPDATE [{tableName}] SET ");
+
+            foreach (var (column, _) in updatingValues)
+            {
+                builder.Append($"[{column}] = @{column}, ");
+            }
+
+            var res = builder.ToString().Substring(0, builder.Length - 2);
+            builder = new StringBuilder($"{res} WHERE ");
+
+            foreach (var (column, _) in equalityConditions)
+            {
+                builder.Append($"[{column}] = @eq{column} AND ");
+            }
+
+            return builder.ToString().Substring(0, builder.Length - 5);
         }
     }
 }
---
+++ Tests/UpdateTests/UpdateByConditionsTests.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Data.SqlClient;
+using EasyADO.NET;
+using NUnit.Framework;
+
+namespace Tests.UpdateTests
+{
+    [TestFixture]
+    public class UpdateByConditionsTests : BaseTestFixture
+    {
+        [SetUp]
+        public void Init()
+        {
+            _easyAdoNet = new EasyAdoNet(ConnectionString);
+        }
+
+        private EasyAdoNet _easyAdoNet;
+
+        private static readonly object[] CorrectParameters =
+        {
+            new object[]
+            {
+                "Persons",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Maksym"),
+                    new Tuple<string, object>("Surname", "Lemich")
+                },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark")
+                }
+            },
+            new object[]
+            {
+                "Roles",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Admin")
+                },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "NotAdmin")
+                }
+            }
+        };
+
+        private static readonly object[] IncorrectParameters =
+        {
+            new object[]
+            {
+                "NotExists",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Admin")
+                },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark"),
+                    new Tuple<string, object>("Surname", "Smith")
+                }
+            },
+            new object[]
+            {
+                "Persons",
+                new Tuple<string, object>[] { },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark")
+                }
+            },
+            new object[]
+            {
+                "Persons",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Admin")
+                },
+                new Tuple<string, object>[] { }
+            }
+        };
+
+        private static readonly object[] NullParameters =
+        {
+            new object[]
+            {
+                null,
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Admin")
+                },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark"),
+                    new Tuple<string, object>("Surname", "Smith")
+                }
+            },
+            new object[]
+            {
+                "Persons",
+                null,
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark"),
+                    new Tuple<string, object>("Surname", "Smith")
+                }
+            },
+            new object[]
+            {
+                "Persons",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Admin")
+                },
+                null
+            },
+            new object[]
+            {
+                "Persons",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Admin"),
+                    null
+                },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark"),
+                    new Tuple<string, object>("Surname", "Smith")
+                }
+            }
+        };
+
+        private static readonly object[] IncorrectSqlParameters =
+        {
+            new object[]
+            {
+                "Persons",
+                new[]
+                {
+                    new Tuple<string, object>("NotExists", "Admin")
+                },
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Mark"),
+                    new Tuple<string, object>("Surname", "Smith")
+                }
+            },
+            new object[]
+            {
+                "Persons",
+                new[]
+                {
+                    new Tuple<string, object>("Name", "Maksym")
+                },
+                new[]
+                {
+                    new Tuple<string, object>("NotExists", "Mark"),
+                    new Tuple<string, object>("Surname", "Smith")
+                }
+            }
+        };
+
+        [Test, TestCaseSource(nameof(IncorrectParameters))]
+        public void When_Update_Throws_ArgumentException(string tableName, Tuple<string, object>[] equalityConditions,
+            params Tuple<string, object>[] updatingValues)
+        {
+            Assert.Throws<ArgumentException>(() => _easyAdoNet.Update(tableName, equalityConditions, updatingValues));
+        }
+
+        [Test, TestCaseSource(nameof(NullParameters))]
+        public void When_Update_Throws_ArgumentNullException(string tableName,
+            Tuple<string, object>[] equalityConditions,
+            params Tuple<string, object>[] updatingValues)
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => _easyAdoNet.Update(tableName, equalityConditions, updatingValues));
+        }
+
+        [Test, TestCaseSource(nameof(IncorrectSqlParameters))]
+        public void When_Update_Throws_SqlException(string tableName,
+            Tuple<string, object>[] equalityConditions,
+            params Tuple<string, object>[] updatingValues)
+        {
+            Assert.Throws<SqlException>(() => _easyAdoNet.Update(tableName, equalityConditions, updatingValues));
+        }
+
+        [Test, TestCaseSource(nameof(CorrectParameters))]
+        public void When_Update_Updates_Record(string tableName, Tuple<string, object>[] equalityConditions,
+            params Tuple<string, object>[] updatingValues)
+        {
+            Assert.DoesNotThrow(() => _easyAdoNet.Update(tableName, equalityConditions, updatingValues));
+        }
+    }
+}
