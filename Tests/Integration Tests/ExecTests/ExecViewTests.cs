6100acd97c56e40136e25891fdfc6044451ab73b
Lemich Maksym <lemichello.m@gmail.com>
2019-09-10 12:47:47 +0300

    feat(Tests): added integration tests for ExecView method

---
+++ Tests/Integration Tests/ExecTests/ExecViewTests.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using EasyADO.NET;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Tests.EntityFramework.Entities;
+
+namespace Tests.Integration_Tests.ExecTests
+{
+    [TestFixture]
+    public class ExecViewTests : BaseTestFixture
+    {
+        [SetUp]
+        public void Init()
+        {
+            _easyAdoNet = new EasyAdoNet(ConnectionString);
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeInit()
+        {
+            Context.Database.ExecuteSqlCommand("CREATE VIEW [Persons Names] AS SELECT Id, Name, Surname FROM Persons;");
+            Context.Database.ExecuteSqlCommand("CREATE VIEW [Empty View] AS SELECT EmptyName FROM EmptyTable;");
+        }
+
+        private EasyAdoNet _easyAdoNet;
+
+        [Test]
+        public void When_ExecViewResult_EqualsTo_EmptyResult()
+        {
+            var actualReader     = _easyAdoNet.ExecView("Empty View");
+            var actualCollection = new List<EmptyTable>();
+
+            while (actualReader.Read())
+            {
+                actualCollection.Add(new EmptyTable
+                {
+                    EmptyName = actualReader[0].ToString()
+                });
+            }
+
+            Assert.IsEmpty(actualCollection);
+        }
+
+        [Test]
+        public void When_ExecViewResult_EqualsTo_ExpectedResult()
+        {
+            var expectedCollection = Context.Persons.FromSql($"SELECT * FROM Persons Names").ToList();
+            var actualReader       = _easyAdoNet.ExecView("Persons Names");
+            var actualCollection   = new List<Person>();
+
+            while (actualReader.Read())
+            {
+                actualCollection.Add(new Person
+                {
+                    Id      = (int) actualReader[0],
+                    Name    = actualReader[1].ToString(),
+                    Surname = actualReader[2].ToString()
+                });
+            }
+
+            Assert.AreEqual(expectedCollection, actualCollection);
+        }
+    }
+}
