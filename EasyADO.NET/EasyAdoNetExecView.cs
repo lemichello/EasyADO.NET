711963f41ae379e320b405c3719460c30e481e2d
Lemich Maksym <lemichello.m@gmail.com>
2019-09-12 14:34:42 +0300

    feat(*): added generic version for ExecView method

--- EasyADO.NET/EasyAdoNetExecView.cs
+++ EasyADO.NET/EasyAdoNetExecView.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using EasyADO.NET.Utils;
 
 namespace EasyADO.NET
 {
@@ -23,6 +25,21 @@ namespace EasyADO.NET
             {
                 return command.ExecuteReader();
             }
+        }
+
+        /// <summary>
+        /// Executes given view and returns all the data from the result.
+        /// </summary>
+        /// <param name="viewName">Name of the view.</param>
+        /// <typeparam name="T">Type of the class, to which will be converted result of query.</typeparam>
+        /// <returns>Collection of <typeparamref name="T"/> instances.</returns>
+        /// <exception cref="ArgumentNullException">Throws, when <paramref name="viewName"/> is null.</exception>
+        /// <exception cref="SqlException">Throws, when <paramref name="viewName"/> doesn't exist in the database.'</exception>
+        public IEnumerable<T> ExecView<T>(string viewName) where T : class, new()
+        {
+            var reader = ExecView(viewName);
+
+            return reader.ConvertToClass<T>();
         }
     }
 }
--- Tests/Integration Tests/ExecTests/ExecViewTests.cs
+++ Tests/Integration Tests/ExecTests/ExecViewTests.cs
@@ -26,6 +26,23 @@ namespace Tests.Integration_Tests.ExecTests
         private EasyAdoNet _easyAdoNet;
 
         [Test]
+        public void When_ExecViewGenericResult_EqualsTo_EmptyResult()
+        {
+            var actualCollection = _easyAdoNet.ExecView<EmptyTable>("Empty View");
+
+            Assert.IsEmpty(actualCollection);
+        }
+
+        [Test]
+        public void When_ExecViewGenericResult_EqualsTo_ExpectedResult()
+        {
+            var expectedCollection = Context.Persons.FromSql($"SELECT * FROM Persons Names").ToList();
+            var actualCollection   = _easyAdoNet.ExecView<Person>("Persons Names");
+
+            Assert.AreEqual(expectedCollection, actualCollection);
+        }
+
+        [Test]
         public void When_ExecViewResult_EqualsTo_EmptyResult()
         {
             var actualReader     = _easyAdoNet.ExecView("Empty View");
